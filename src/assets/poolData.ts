7981e0384efa699cd7219f04431c35189b38867e
DanLowo <56523775+DanLowo@users.noreply.github.com>
2023-02-02 19:31:09 +0100

    Design: Navigation Bar & Trending Cards
    * css variables
    
    * css utils: buttons, media queries
    
    * css flex utils
    
    * configure styles
    
    * add font awesome icon
    
    * design: navigation bar (with responsiveness)
    
    * clean up default styles
    
    * design: pool card & pool card container
    
    * pool card data & images
    
    * clean up app and insert components
    
    * npm package

---
+++ src/assets/poolData.ts
@@ -0,0 +1,11 @@
+import SolanaLogo from "./Solana_logo.png"
+import USDCLogo from "./usd-coin.png"
+
+const poolData = [
+    { name: "SOL/USD", image1: SolanaLogo, image2: USDCLogo, supply: "2,534,000", apr: 12 },
+    { name: "BTC/USD", image1: SolanaLogo, image2: USDCLogo, supply: "2,534,000", apr: 18 },
+    { name: "AVAX/USD", image1: SolanaLogo, image2: USDCLogo, supply: "2,534,000", apr: 7 },
+    { name: "ADA/USD", image1: SolanaLogo, image2: USDCLogo, supply: "2,534,000", apr: 8.5 },
+]
+
+export default poolData
