9e6bf5b1cc8075d36495b65ba4c6e11b9df5d66e
DanLowo <56523775+DanLowo@users.noreply.github.com>
2023-02-02 19:31:09 +0100

    Merge pull request #1
    * css variables
    
    * css utils: buttons, media queries
    
    * css flex utils
    
    * configure styles
    
    * add font awesome icon
    
    * design: navigation bar (with responsiveness)
    
    * clean up default styles
    
    * design: pool card & pool card container
    
    * pool card data & images
    
    * clean up app and insert components
    
    * npm package

---
+++ src/components/nav/Navigation.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+</script>
+
+
+<template>
+  <nav class="navigation">
+    <div class="space_between">
+      <h1>RADIANCE</h1>
+      <div class="row_flex_end">
+        <button class="button">
+          <i class="fal fa-wallet"></i>
+          <span>
+            Connect
+            <span class="wallet">wallet</span>
+          </span>
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<style scoped>
+  nav.navigation {
+    padding: .4rem var(--gutter);
+    background: var(--primaryColor);
+    width: calc(100% - calc(var(--gutter) * 2));
+  }
+
+  .space_between div {
+    width: 20%;
+  }
+
+  .button {
+    width: fit-content;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .space_between div {
+      width: 50%;
+    }
+
+    .button {
+      padding: .6rem .7rem;
+      border-radius: .3rem;
+    }
+
+    .button span {
+      bottom: -.125rem;
+      position: relative;
+    }
+
+    .button span.wallet {
+      display: none;
+    }
+  }
+
+</style>
---
+++ src/components/poolCard/PoolCard.vue
@@ -0,0 +1,132 @@
+<script setup lang="ts">
+defineProps<{
+  token1Image: object,
+  token2Image: object,
+  pairName: string,
+  apr: number,
+  supply: string
+}>()
+</script>
+
+
+<template>
+  <div class="pool_card flex_column">
+    <h3 class="pool_name">{{pairName}}</h3>
+
+    <div class="pool_image flex_row_center">
+      <img :src="token1Image" :alt="pairName" />
+      <img :src="token2Image" :alt="pairName" />
+    </div>
+
+    <div class="more_info desktop_only flex_row">
+      <div class="more_info_details">
+        <span>Supply:</span> ${{supply}}
+      </div>
+
+      <div class="more_info_details">
+        <span>S. APR:</span> {{apr}}%
+      </div>
+
+      <div class="more_info_details">
+        <span>Borrowed:</span> ${{supply}}
+      </div>
+
+      <div class="more_info_details">
+        <span>B. APR:</span> {{apr}}%
+      </div>
+    </div>
+
+    <div class="more_info mobile_only flex_row">
+      <div class="more_info_details">
+        <span>Supply APR:</span> {{apr}}%
+      </div>
+
+      <div class="more_info_details">
+        <span>Borrow APR:</span> {{apr}}%
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+
+.pool_card:hover {
+  cursor: pointer;
+  transition: all ease-in 0.2s;
+  transform: scale(1.03);
+}
+
+.pool_name {
+  font-size: 1.3rem;
+}
+
+.pool_card.flex_column {
+  width: calc(25% - 2.8rem);
+  padding: 0 .6rem 1rem;
+  border-radius: .4rem;
+  background: var(--primaryColor);
+}
+
+.pool_image {
+  padding-left: 1.7rem;
+}
+
+.pool_image img {
+  padding: .2rem;
+  background: var(--hoverColor);
+}
+
+.pool_image img:first-of-type {
+  position: relative;
+  /*z-index: 2;*/
+}
+
+.pool_image img:last-of-type {
+  position: relative;
+  left: -2.5rem;
+}
+
+.pool_image img {
+  width: 110px;
+  height: 110px;
+  border-radius: 50%;
+}
+
+@media only screen and (max-width: 600px) {
+  .pool_card.flex_column {
+    width: 43%;
+    padding-bottom: .5rem;
+  }
+
+  .pool_image {
+    padding-left: .5rem;
+  }
+
+  .pool_image img {
+    width: 55px;
+    height: 55px;
+  }
+
+  .pool_image img:last-of-type {
+    left: -1rem;
+  }
+
+  .more_info {
+    /*display: none;*/
+  }
+}
+
+.more_info {
+  gap: .4rem .3rem;
+  margin-top: 1.5rem;
+  color: var(--hoverColor);
+}
+
+.more_info_details {
+  font-size: .85rem;
+  padding: .1rem .6rem;
+  background: #4b2630;
+  border-radius: .4rem;
+}
+</style>
