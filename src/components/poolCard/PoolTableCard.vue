33c958d0bbd8077cb30f0a9db5a23edad23f2ae8
danlowo <danielolulowo3@gmail.com>
2023-02-04 01:55:31 +0100

    implement: table & pool card with mobile responsiveness

---
+++ src/components/poolCard/PoolTableCard.vue
@@ -0,0 +1,129 @@
+<script setup lang="ts">
+import Sol from "../../assets/Solana_logo.png"
+import USDC from "../../assets/usd-coin.png"
+</script>
+
+
+<template>
+  <div class="table_pool_card">
+    <div class="table_headers flex_row">
+      <div class="pool_name flex_row">
+        <div class="flex_row_center">
+          <img :src="Sol" width="34" alt="token" />
+          <img :src="USDC" width="34" alt="token" />
+        </div>
+        <h4>SOL/USD</h4>
+      </div>
+      <div class="supply">
+        <h5>$4,724,544</h5>
+      </div>
+      <div>
+        <h5>$143,104</h5>
+      </div>
+      <div>
+        <h5>8.74%</h5>
+      </div>
+      <div>
+        <h5>11.92%</h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+  h2, h4, h5, p {
+    padding: 0;
+    margin: 0;
+  }
+
+  .table_pool_card {
+    padding: 1.5rem 1rem;
+    border-radius: var(--radius);
+    width: calc(100% - 2rem);
+    background: rgba(0, 0, 0, 0.34);
+  }
+
+  .table_headers {
+    border-radius: var(--smRadius);
+    width: calc(100% - 2rem);
+    padding: 0 1rem;
+    color: var(--darkText);
+
+    h5 {
+      font-size: .9rem;
+    }
+
+    > div:first-of-type {
+      width: 32% !important;
+    }
+
+    > div {
+      display: flex;
+      width: calc(17% - .5rem);
+    }
+
+    div {
+      gap: .5rem;
+      align-items: center;
+      display: flex;
+    }
+  }
+
+  .pool_name {
+    h4 {
+      top: .1rem;
+      left: -.8rem;
+      position: relative;
+    }
+
+    .flex_row_center {
+      //width: 100%;
+      img {
+        padding: .1rem;
+        display: inline-block;
+        border-radius: 50%;
+        background: var(--hoverColor);
+      }
+
+      img:first-of-type {
+        position: relative;
+        /*z-index: 2;*/
+      }
+
+      img:last-of-type {
+        position: relative;
+        left: -1.2rem;
+      }
+
+    }
+  }
+
+
+  @media only screen and (max-width: 600px) {
+    .table_headers {
+      padding: 0;
+      width: 100%;
+      justify-content: space-between;
+      > div {
+        display: none;
+      }
+
+      > div:first-of-type {
+        width: 60% !important;
+        h4 {
+          left: -1rem;
+        }
+      }
+
+      .pool_name, .supply {
+        width: 35% !important;
+        display: flex;
+      }
+
+      .supply {
+        justify-content: flex-end;
+      }
+    }
+  }
+</style>
---
+++ src/components/tables/Table.vue
@@ -0,0 +1,145 @@
+<script setup lang="ts">
+// defineProps<{ msg: string }>()
+</script>
+
+
+<template>
+  <div class="table_container">
+    <div class="table">
+
+      <div class="space_between">
+        <div class="flex_column title">
+          <h2>Lending Pools</h2>
+          <p>Earn yield on trading fees by providing liquidity</p>
+        </div>
+
+        <div class="input_container">
+          <i class="fal fa-search"></i>
+          <input class="input" placeholder="Search pools" />
+        </div>
+      </div>
+
+      <div class="table_headers space_between">
+        <div class="pool_name flex_row_center">
+          <h5>Pool Name</h5>
+        </div>
+        <div class="supply">
+          <h5>Total Supply</h5>
+          <i class="fad fa-sort-up"></i>
+        </div>
+        <div>
+          <h5>Total Borrowed</h5>
+          <i class="fad fa-sort-up"></i>
+        </div>
+        <div>
+          <h5>Supply APR</h5>
+          <i class="fad fa-sort-up"></i>
+        </div>
+        <div>
+          <h5>Borrow APR</h5>
+          <i class="fad fa-sort-up"></i>
+        </div>
+      </div>
+
+      <div class="cards flex_column">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+  h2, h5, p {
+    padding: 0;
+    margin: 0;
+  }
+
+  .table_container {
+    padding: var(--gutter);
+    width: calc(100% - var(--gutter) * 2);
+  }
+
+  .table {
+    padding: 2rem var(--gutter);
+    min-height: 30vh;
+    border-radius: var(--radius);
+    background: var(--primaryColor);
+  }
+
+  .table {
+    .title {
+      gap: .1rem;
+      h2 {
+        font-size: 1.3rem;
+      }
+      p {
+        font-size: .9rem;
+        color: var(--darkText);
+      }
+    }
+  }
+
+  .table_headers {
+    margin-top: 2rem;
+    border-radius: var(--smRadius);
+    background: rgba(0, 0, 0, 0.12);
+    width: calc(100% - 4rem);
+    padding: 1rem 2rem;
+    color: var(--darkText);
+
+    h5 {
+      font-size: .9rem;
+    }
+
+    > div:first-of-type {
+      justify-content: flex-start;
+      width: 32% !important;
+    }
+
+    > div {
+      display: flex;
+      //justify-content: center;
+      width: calc(17% - .5rem);
+    }
+
+    div {
+      gap: .5rem;
+      align-items: center;
+      display: flex;
+    }
+  }
+
+  .cards {
+    margin-top: 1rem;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .input_container {
+      width: 100%;
+      margin-top: 1rem;
+      input {
+        width: calc(100% - 2.8rem);
+      }
+    }
+
+    .table_headers {
+      padding: 1rem;
+      width: calc(100% - 2rem);
+      justify-content: space-between;
+      > div {
+        display: none;
+      }
+
+      .pool_name, .supply {
+        width: 45%;
+        display: flex;
+      }
+
+      .supply {
+        justify-content: flex-end;
+      }
+    }
+  }
+
+</style>
