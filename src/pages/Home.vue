f2a05ab06bf83a3d7e5857508bfa123766502c8c
Sol <oluwafemioyepeju@gmail.com>
2023-02-04 10:10:43 +0100

    renamed to PoolDetails

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
     import PoolCardContainer from "../components/poolCard/PoolCardContainer.vue";
+    import PoolTableCard from "../components/poolCard/PoolTableCard.vue";
     import poolData from "../assets/poolData";
 </script>
 
@@ -8,4 +9,10 @@
         :poolData="poolData"
         sectionTitle="Trending Pools"
     />
+    <Table>
+      <PoolTableCard />
+      <PoolTableCard />
+      <PoolTableCard />
+      <PoolTableCard />
+    </Table>
 </template>
